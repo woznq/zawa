7c1d218504362fefb9e8bb29d2db10c621033ea9
student <jakubw835@gmail.com>
2023-03-09 15:58:28 +0100

    ojojoj

--- src/SvgScene.java
+++ src/SvgScene.java
@@ -1,19 +1,33 @@
 package com.company;
 
 import java.awt.*;
+import java.io.FileWriter;
+import java.io.IOError;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Locale;
 
 
 public class SvgScene {
     private List<Polygon> l = new ArrayList<>();
+    private double width=500,height=500;
     public void addPolygon(Polygon polygon)
     {
         l.add(polygon);
     }
-    public void save(String path)
+    public void save(String path){
+    try{
+        FileWriter file=new FileWriter(path);
+        file.write("<html>/n </html>");
+        file.write(String.format(Locale.ENGLISH,"<svg height=\"%f\" width=\"%f\"",height,width  );
+        for(Polygon p:l)
+        {
+            file.write("\t"+p.toSvg());
+        }
+        file.write("</svg>\n</body>\n</html>");
+    }catch(IOException e)
     {
-
+        e.printStackTrace();
     }
 }
